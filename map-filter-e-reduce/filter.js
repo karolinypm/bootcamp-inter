29f7c84c315c1aadecf4d9ff0da5cfb6fcb48992
karolip.matias <karolinyp.matias@gmail.com>
2022-01-21 21:47:14 -0300

    atividade prática para aprendizado de map, filter e reduce

---
+++ map-filter-e-reduce/filter.js
@@ -0,0 +1,12 @@
+//Filtre e retorne todos os números pares de um array.
+function filtraPares(arr){
+  return arr.filter(callback);
+}
+
+function callback(item){
+  return item % 2 === 0;
+}
+
+const meuArray = [1,23,55,57,30,2,4];
+
+console.log(filtraPares(meuArray));
